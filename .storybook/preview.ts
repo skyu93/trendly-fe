fbc347975da49828b51cb550539cfae5407e2aa3
kyu <seungkyu0215@clobot.co.kr>
2025-02-17 06:51:49 +0900

    초기 커밋

---
+++ .storybook/preview.ts
@@ -0,0 +1,19 @@
+import type { Preview } from '@storybook/react';
+import '../src/app/globals.css'; // 전역 스타일 적용
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+};
+
+export default preview;
---
+++ src/app/layout.tsx
@@ -0,0 +1,53 @@
+import './reset.css';
+import './globals.css';
+import type { Metadata, Viewport } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import { ReactNode } from 'react';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Trendly',
+  description: '당신의 생각이 트렌드가 되는 순간',
+  keywords: '이슈, 트렌드, 키워드',
+  openGraph: {
+    title: 'Trendly',
+    description: '당신의 생각이 트렌드가 되는 순간',
+    type: 'website',
+    url: 'https://example.com',
+    images: [
+      {
+        url: 'https://example.com/og-image.jpg',
+        width: 800,
+        height: 600,
+      },
+    ],
+  },
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
