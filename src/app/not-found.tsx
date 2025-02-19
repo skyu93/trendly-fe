348553d415bfb04811cf3b0f1fff6ee2188e7bb1
kyu <seungkyu0215@clobot.co.kr>
2025-02-19 23:10:14 +0900

    not-found 페이지 레이아웃 작업

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,18 @@
+import Link from 'next/link';
+import { ROUTE_PATH } from '@/constants/route';
+
 export default function notFound() {
-  return <div>Not found</div>;
+  return (
+    <div className="w-full h-screen relative flex flex-col items-center justify-center">
+      <div className="w-32 h-32 mx-auto mb-4 animate-pulse">
+        {/* 로고나 아이콘을 넣을 위치 */}
+        <div className="w-full h-full bg-[#EAFF4C] rounded-lg" />
+      </div>
+      <p className="text-xl font-bold">Oops, 페이지를 찾을 수 없어요.</p>
+      <p className="text-gray-400 text-sm">메인으로 돌아가서 키워드 순위를 확인해보세요!</p>
+      <Link className="bg-gray-300 text-black font-bold mt-20 p-3 rounded-md" href={ROUTE_PATH.MAIN}>
+        메인으로 돌아가기
+      </Link>
+    </div>
+  );
 }
