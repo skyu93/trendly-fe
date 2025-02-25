e4466f440ef1b8e8e9ff6425c10777a475c6cd22
skyu93 <seungkyu0215@gmail.com>
2025-02-25 17:33:38 +0900

    feat: 메인 키워드 페이지 레이아웃 작업

---
+++ src/app/keywords/KeywordRankList.tsx
@@ -0,0 +1,37 @@
+import { Separator } from '@/components/ui/separator';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import KeywordRankCard from '@/app/keywords/keywordRankCard';
+import SvgIcon from '@/components/SvgIcon';
+
+interface Props {
+  title: string;
+  list: {
+    rank: number;
+    keyword: string;
+  }[];
+}
+export default function KeywordRankList({ title, list }: Props) {
+  return (
+    <div className="flex flex-col px-4 w-full h-full text-greyscale-10">
+      <div className="flex items-center justify-between py-8">
+        <div className="flex items-center gap-1">
+          <SvgIcon id="trend" color="primary" />
+          <div>{title}</div>
+        </div>
+        <div className="flex items-center gap-1">
+          <div className="text-greyscale-20 text-sm">기간설정</div>
+          <SvgIcon id="filter" size={12} color="greyscale-20" />
+        </div>
+      </div>
+
+      <ScrollArea className="flex-1">
+        {list.map(({ rank, keyword }) => (
+          <div key={keyword}>
+            <KeywordRankCard rank={rank} keyword={keyword} />
+            <Separator className="bg-greyscale-80" />
+          </div>
+        ))}
+      </ScrollArea>
+    </div>
+  );
+}
---
+++ src/app/keywords/keywordRankCard.tsx
@@ -0,0 +1,26 @@
+import { Button } from '@/components/ui/button/Button';
+import SvgIcon from '@/components/SvgIcon';
+
+interface Props {
+  rank: number;
+  keyword: string;
+}
+export default function KeywordRankCard({ rank, keyword }: Props) {
+  return (
+    <div className="group flex items-center justify-between h-[67px] py-[18px] px-3 rounded-lg hover:cursor-pointer hover:bg-greyscale-20">
+      <div className="flex items-center">
+        <div className="w-6 h-6 rounded-full bg-greyscale-80 text-greyscale-10 flex items-center justify-center group-hover:bg-greyscale-10 group-hover:text-greyscale-90">
+          {rank}
+        </div>
+        <span className="ml-[6px]">{keyword}</span>
+      </div>
+      <Button
+        variant="outline"
+        className="py-2 px-3 border-greyscale-80 text-greyscale-20 group-hover:pointer-events-none group-hover:text-primary-60 group-hover:bg-greyscale-80"
+      >
+        채팅하기
+        <SvgIcon id="add-chat" />
+      </Button>
+    </div>
+  );
+}
