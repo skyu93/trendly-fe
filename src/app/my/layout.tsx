c47e7d02f9304744701fb9762b978d18fc9b3b7d
skyu93 <seungkyu0215@gmail.com>
2025-03-08 12:42:48 +0900

    마이페이지 글로벌 탭바 추가

--- src/app/my/layout.tsx
+++ src/app/my/layout.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from 'react';
 import MyHeader from '@/app/my/MyHeader';
+import { GlobalFooterNavi } from '@/app/GlobalFooterNavi';
 
 export default function MyLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <>
       <MyHeader />
       {children}
+      <GlobalFooterNavi />
     </>
   );
 }
