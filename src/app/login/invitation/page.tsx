5e55cc1d2f6198d188de65053e41a7a7ff48e4bf
skyu93 <seungkyu0215@gmail.com>
2025-03-11 19:03:45 +0900

    로그인 유도 페이지 x버튼 추가

--- src/app/login/invitation/page.tsx
+++ src/app/login/invitation/page.tsx
@@ -2,11 +2,20 @@
 import KakaoButton from '@/app/login/kakaoButton';
 import Image from 'next/image';
 import logo from '@/assets/logo.svg';
+import Link from 'next/link';
+import { ROUTE_PATH } from '@/constants/route';
+import { X } from 'lucide-react';
 
 export default function LoginPage() {
   return (
     <div className="w-full h-screen px-6 relative flex flex-col items-center justify-center">
-      <Image src={logo} alt="logo" className="" />
+      <Link
+        href={ROUTE_PATH.KEYWORDS}
+        className="absolute top-2 left-6 flex items-center justify-end text-sm hover:cursor-pointer hover:font-bold"
+      >
+        <X className="w-6 h-6 text-greyscale-20 hover:text-greyscale-50" />
+      </Link>
+      <Image src={logo} alt="logo" />
       <div className="w-full flex flex-col items-center">
         <span className="text-2xl font-bold my-3">지금 핫한 트렌드를 알아볼까요?</span>
         <div className="flex flex-col items-center text-sm text-greyscale-50">
