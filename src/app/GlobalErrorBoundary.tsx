72c1e146af069cb2c96d785fbab9ce01fa47ec29
skyu93 <seungkyu0215@gmail.com>
2025-03-12 07:18:42 +0900

    ErrorHandler zustand로 변경

--- src/app/GlobalErrorBoundary.tsx
+++ src/app/GlobalErrorBoundary.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useCallback, useEffect } from 'react';
 import {
   AlertDialog,
   AlertDialogAction,
@@ -11,9 +11,42 @@ import {
   AlertDialogTitle,
 } from '@/components/ui/alert-dialog';
 import { useErrorHandler } from '@/hooks/useErrorHandler';
+import { ApiError } from '@/services/apiError';
+import { ERROR_CODES } from '@/constants/errorCodes';
+import { ROUTE_PATH } from '@/constants/route';
+import { useRouter } from 'next/navigation';
 
 export default function GlobalErrorBoundary({ children }: { children: ReactNode }) {
-  const { isError, setError } = useErrorHandler();
+  const { isError, error, setError } = useErrorHandler();
+  const router = useRouter();
+
+  const handleErrorRouter = useCallback(() => {
+    if (error instanceof ApiError) {
+      const { code } = error;
+
+      switch (code) {
+        case ERROR_CODES.TOKEN_INVALID:
+          // 토큰 만료 처리
+          router.push(ROUTE_PATH.LOGIN_INVITATION);
+          break;
+        case ERROR_CODES.LOGIN_FAILED:
+          // 로그인 실패 처리
+          router.push(ROUTE_PATH.LOGIN);
+          break;
+        default:
+          // 기타 오류 처리
+          console.error(error);
+          break;
+      }
+    } else if (error) {
+      console.error(error);
+    }
+  }, [router, error]);
+
+  useEffect(() => {
+    handleErrorRouter();
+  }, [error]);
+
   return (
     <>
       <AlertDialog open={isError}>
--- src/hooks/useErrorHandler.ts
+++ src/hooks/useErrorHandler.ts
@@ -1,41 +1,41 @@
-import { useRouter } from 'next/navigation';
+import { create } from 'zustand';
 import { ApiError } from '@/services/apiError';
-import { ERROR_CODES } from '@/constants/errorCodes';
-import { ROUTE_PATH } from '@/constants/route';
-import { useMemo, useState } from 'react';
 import { AxiosError } from 'axios';
 import { isNotNil } from 'es-toolkit';
 
 type ErrorInstance = ApiError | AxiosError | Error | unknown;
-export const useErrorHandler = () => {
-  const router = useRouter();
-  const [error, setError] = useState<ErrorInstance | null>(null);
 
-  const isError = useMemo(() => isNotNil(error), [error]);
-  const handleError = (error: ErrorInstance | null) => {
-    if (error instanceof ApiError) {
-      const { code } = error;
-      switch (code) {
-        case ERROR_CODES.TOKEN_INVALID:
-          {
-            // 토큰 만료
-            router.push(ROUTE_PATH.LOGIN_INVITATION);
-          }
-          break;
-        default:
-          // 기타 오류 처리
-          console.error('API 오류:', error);
-          break;
-      }
-    }
+interface ErrorState {
+  error: ErrorInstance | null;
+  isError: boolean;
+  setError: (error: ErrorInstance | null) => void;
+  handleError: (errorInstance: ErrorInstance | null) => void;
+  reset: () => void;
+}
 
-    setError(error);
-    return error;
-  };
+export const useErrorHandler = create<ErrorState>(set => ({
+  error: null,
+  isError: false,
 
-  return {
-    handleError,
-    isError,
-    setError,
-  };
-};
+  setError: (errorInstance: ErrorInstance | null) => {
+    set({
+      error: errorInstance,
+      isError: isNotNil(errorInstance),
+    });
+  },
+
+  handleError: (errorInstance: ErrorInstance | null) => {
+    // 상태 업데이트
+    set({
+      error: errorInstance,
+      isError: isNotNil(errorInstance),
+    });
+  },
+
+  reset: () => {
+    set({
+      error: null,
+      isError: false,
+    });
+  },
+}));
