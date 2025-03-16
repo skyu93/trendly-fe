6ec0cfb6bf01ab4257889f8fac676b752d0d06bf
skyu93 <seungkyu0215@gmail.com>
2025-03-17 07:15:13 +0900

    로그아웃 에러 처리

--- src/app/(auth)/my/LogoutButton.tsx
+++ src/app/(auth)/my/LogoutButton.tsx
@@ -3,13 +3,19 @@ import { useAuth } from '@/hooks/auth/useAuth';
 import { Button } from '@/components/ui/button/Button';
 import { useRouter } from 'next/navigation';
 import { ROUTE_PATH } from '@/constants/route';
+import { useErrorHandler } from '@/hooks/useErrorHandler';
 
 export default function LogoutButton() {
   const { logout } = useAuth();
   const router = useRouter();
+  const { handleError } = useErrorHandler();
   const handleLogout = () => {
-    logout();
-    router.push(ROUTE_PATH.LOGIN);
+    try {
+      logout();
+      router.push(ROUTE_PATH.LOGIN);
+    } catch (error) {
+      handleError(error);
+    }
   };
   return (
     <Button variant="outline" className="mb-6" onClick={handleLogout}>
