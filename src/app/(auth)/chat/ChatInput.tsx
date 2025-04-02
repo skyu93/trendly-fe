7f5c2b7dd254f106634f1f9a500f796c5f1914b2
skyu93 <seungkyu0215@gmail.com>
2025-04-02 22:12:39 +0900

    bugfix: 엔터 입력시 2번 입력되는 버그 수정

--- src/app/(auth)/chat/ChatInput.tsx
+++ src/app/(auth)/chat/ChatInput.tsx
@@ -1,14 +1,14 @@
 'use client';
 
-import React, { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from 'react';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { ArrowUp } from 'lucide-react';
 import { Button } from '@/components/ui/button/Button';
 
-interface ChatInputProps {
+interface Props {
   onSendMessage: (message: string) => void;
 }
 
-const ChatInput: React.FC<ChatInputProps> = ({ onSendMessage }) => {
+const ChatInput: React.FC<Props> = ({ onSendMessage }) => {
   const [message, setMessage] = useState<string>('');
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -31,21 +31,22 @@ const ChatInput: React.FC<ChatInputProps> = ({ onSendMessage }) => {
     setMessage(e.target.value);
   };
 
-  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>): void => {
-    if (e.key === 'Enter') {
+  const handleKeyPress = (e: React.KeyboardEvent): void => {
+    if (e.key === 'Enter' && !e.shiftKey && !e.repeat) {
+      e.preventDefault();
+      console.log('handleKeyPress - Enter');
       handleSend();
     }
   };
 
   return (
-    <div className="p-4 border-greyscale-70">
+    <div className="p-4 border-greyscale-70" onKeyPress={handleKeyPress}>
       <div className="flex items-center">
         <input
           ref={inputRef}
           type="text"
           value={message}
           onChange={handleInputChange}
-          onKeyDown={handleKeyDown}
           placeholder="내용을 입력해 주세요"
           className="flex-1 p-2 mx-2 rounded-full bg-greyscale-80 text-white focus:outline-none focus:ring-2 focus:ring-blue-400"
           maxLength={500}
